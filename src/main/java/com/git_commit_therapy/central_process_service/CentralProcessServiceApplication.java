8b83ec5e35281db113860a0c6dfd7a1362c36e39
AndreaDaLio <884046@stud.unive.it>
2024-12-10 21:00:43 +0100

    First commit

---
+++ src/main/java/com/git_commit_therapy/central_process_service/CentralProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.git_commit_therapy.central_process_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralProcessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_commit_therapy/central_process_service/CentralProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_commit_therapy.central_process_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
