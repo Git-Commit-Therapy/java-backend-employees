373a57586cee56ece95167e4d1c7b614523ee277
AndreaDaLio <884046@stud.unive.it>
2025-01-07 22:10:12 +0100

    Fix merge problems

--- src/main/java/com/git_commit_therapy/employeeService/EmployeeServiceApplication.java
+++ src/main/java/com/git_commit_therapy/employeeService/EmployeeServiceApplication.java
@@ -7,16 +7,10 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-<<<<<<< HEAD:src/main/java/com/git_commit_therapy/central_process_service/CentralProcessServiceApplication.java
-@ComponentScan({"com.git_commit_therapy.employeeService","com.git_commit_therapy.emergency"})
+@ComponentScan({"com.git_commit_therapy.employeeService", "com.git_commit_therapy.emergency"})
 @EnableScheduling
-public class CentralProcessServiceApplication {
-=======
 public class EmployeeServiceApplication {
->>>>>>> 29dce637771feba44333a74ed3d75cdf39aca31c:src/main/java/com/git_commit_therapy/employeeService/EmployeeServiceApplication.java
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
 }
