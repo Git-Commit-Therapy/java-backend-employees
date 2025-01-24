b152cdeb61eecef849c0089800d717853fc09ab5
Andrea Moruzzi <andrea.moruzzi02@gmail.com>
2025-01-24 18:21:27 +0100

    Added Entity Manager

--- src/main/java/com/git_commit_therapy/employeeService/dao/DoctorDao.java
+++ src/main/java/com/git_commit_therapy/employeeService/dao/DoctorDao.java
@@ -2,8 +2,11 @@ package com.git_commit_therapy.employeeService.dao;
 
 import com.git_commit_therapy.employeeService.entity.Doctor;
 import com.git_commit_therapy.employeeService.repository.DoctorRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +15,9 @@ import java.util.Optional;
 public class DoctorDao {
 
     private final DoctorRepository doctorRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public DoctorDao(DoctorRepository doctorRepository) {
@@ -26,7 +32,15 @@ public class DoctorDao {
         return doctorRepository.findAll();
     }
 
+    @Transactional
     public Doctor upsert(Doctor doctor) {
-        return doctorRepository.save(doctor);
+        //return doctorRepository.save(doctor);
+        entityManager.persist(doctor.getUser());
+        entityManager.flush();
+        entityManager.merge(doctor.getWard());
+        entityManager.flush();
+        entityManager.persist(doctor);
+        entityManager.flush();
+        return doctor;
     }
 }
--- src/main/java/com/git_commit_therapy/employeeService/dao/PatientDao.java
+++ src/main/java/com/git_commit_therapy/employeeService/dao/PatientDao.java
@@ -2,8 +2,11 @@ package com.git_commit_therapy.employeeService.dao;
 
 import com.git_commit_therapy.employeeService.entity.Patient;
 import com.git_commit_therapy.employeeService.repository.PatientRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +15,9 @@ import java.util.Optional;
 public class PatientDao {
 
     private final PatientRepository patientRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public PatientDao(PatientRepository patientRepository) {
@@ -26,7 +32,13 @@ public class PatientDao {
         return patientRepository.findAll();
     }
 
+    @Transactional
     public Patient upsert(Patient patient) {
-        return patientRepository.save(patient);
+        //return patientRepository.saveAndFlush(patient);
+        entityManager.persist(patient.getUser());
+        entityManager.flush();
+        entityManager.persist(patient);
+        entityManager.flush();
+        return patient;
     }
 }
